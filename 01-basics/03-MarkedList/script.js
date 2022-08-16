be0c930e3c8347b11969607c11d480e42672ff5a
tyshenko61 <34244584+tyshenko61@users.noreply.github.com>
2022-08-16 20:12:49 +0500

    список Email-ов из массива emails

--- 01-basics/03-MarkedList/script.js
+++ 01-basics/03-MarkedList/script.js
@@ -1,32 +1,53 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
+createApp({
+  data() {
+    return {
+      emails : [
+        'Eliseo@gardner.biz',
+        'Jayne_Kuhic@sydney.com',
+        'Nikita@garfield.biz',
+        'Lew@alysha.tv',
+        'Hayden@althea.biz',
+        'Presley.Mueller@myrl.com',
+        'Dallas@ole.me',
+        'Mallory_Kunze@marie.org',
+        'Meghan_Littel@rene.us',
+        'Carmen_Keeling@caroline.name',
+        'Veronica_Goodwin@timmothy.net',
+        'Oswald.Vandervort@leanne.org',
+        'Kariane@jadyn.tv',
+        'Nathan@solon.io',
+        'Maynard.Hodkiewicz@roberta.com',
+        'Christine@ayana.info',
+        'Preston_Hudson@blaise.tv',
+        'Vincenza_Klocko@albertha.name',
+        'Madelynn.Gorczany@darion.biz',
+        'Mariana_Orn@preston.org',
+        'Noemie@marques.me',
+        'Khalil@emile.co.uk',
+        'Sophia@arianna.co.uk',
+        'Jeffery@juwan.us',
+        'Isaias_Kuhic@jarrett.net',
+      ],
+      search: "",
+    };
+  },
+
+ computed: {
+   filteredEmails() {
+    let emailFilter = [];
+    this.emails.forEach(element => {
+      let obj = {email: element, ismarked: false}
+      if (this.search && element.toLowerCase()
+      .includes(this.search.toLowerCase())) {
+        obj.ismarked = true;
+      }
+      emailFilter.push(obj);
+    });
+    return emailFilter;
+    }
+ }
+}).mount('#app');
 
 // From https://jsonplaceholder.typicode.com/comments
-const emails = [
-  'Eliseo@gardner.biz',
-  'Jayne_Kuhic@sydney.com',
-  'Nikita@garfield.biz',
-  'Lew@alysha.tv',
-  'Hayden@althea.biz',
-  'Presley.Mueller@myrl.com',
-  'Dallas@ole.me',
-  'Mallory_Kunze@marie.org',
-  'Meghan_Littel@rene.us',
-  'Carmen_Keeling@caroline.name',
-  'Veronica_Goodwin@timmothy.net',
-  'Oswald.Vandervort@leanne.org',
-  'Kariane@jadyn.tv',
-  'Nathan@solon.io',
-  'Maynard.Hodkiewicz@roberta.com',
-  'Christine@ayana.info',
-  'Preston_Hudson@blaise.tv',
-  'Vincenza_Klocko@albertha.name',
-  'Madelynn.Gorczany@darion.biz',
-  'Mariana_Orn@preston.org',
-  'Noemie@marques.me',
-  'Khalil@emile.co.uk',
-  'Sophia@arianna.co.uk',
-  'Jeffery@juwan.us',
-  'Isaias_Kuhic@jarrett.net',
-];
-
 // Требуется создать Vue приложение
