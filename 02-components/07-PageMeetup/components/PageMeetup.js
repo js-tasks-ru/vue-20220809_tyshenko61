fe4295a2d8b8965035e66f6c539191b41ff865b2
tyshenko61 <34244584+tyshenko61@users.noreply.github.com>
2022-08-19 15:14:52 +0500

    Компоненты 7

--- 02-components/07-PageMeetup/components/PageMeetup.js
+++ 02-components/07-PageMeetup/components/PageMeetup.js
@@ -1,7 +1,9 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
 import UiContainer from './UiContainer.js';
 import UiAlert from './UiAlert.js';
-// import { fetchMeetupById } from './meetupService.js';
+import { fetchMeetupById } from '../meetupService.js';
+import MeetupView from '../../06-MeetupView/components/MeetupView.js';
+
 
 export default defineComponent({
   name: 'PageMeetup',
@@ -9,18 +11,69 @@ export default defineComponent({
   components: {
     UiAlert,
     UiContainer,
+    MeetupView,
   },
+
+  props: {
+    meetupId: {
+      type: Number,
+      required: true,
+    }
+  },
+
+  data() {
+    return {
+      meetup: null,
+      error: null,
+    }
+  },
+
+  // emits: ['update:meetupId'],
+
+  methods: {
+    load(value) {
+      this.error = null;
+      this.meetup =null;
+      fetchMeetupById(value).then(
+        (meetup) => {this.meetup = meetup;
+        },
+        (error) => {this.error = error.message}
+      );
+    }
+  },
+
+  mounted() {
+  //       fetchMeetupById(this.meetupId).then(
+  //         (meetup) => {this.meetup = meetup;
+  //                     }
+  //       );
+    this.load(this.meetupId);
+    },
+
+    watch: {
+      meetupId: {
+        handler(newValue) {
+          this.load(newValue);
+          // fetchMeetupById(newValue).then(
+          //   (meetup) => {this.meetup = meetup;
+          //     this.error = null;
+          //   },
+          //   (error) => {this.error = error.message}
+          // );
+        }
+       }
+     },
 
   template: `
     <div class="page-meetup">
-      <!-- meetup view -->
-
-      <UiContainer>
+      <MeetupView v-if="meetup && !error" :meetup="this.meetup" />
+      <UiContainer v-else-if="!error">
         <UiAlert>Загрузка...</UiAlert>
       </UiContainer>
 
-      <UiContainer>
-        <UiAlert>error</UiAlert>
+      <UiContainer v-else>
+        <UiAlert>{{ error }}</UiAlert>
       </UiContainer>
     </div>`,
 });
+
